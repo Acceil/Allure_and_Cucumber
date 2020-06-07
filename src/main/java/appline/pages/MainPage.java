d1f0f71797e43cece8d20085b056f9a74a0a508d
Acceil <xvorostenok@gmail.com>
2020-06-07 11:55:24 +0300

    Update

--- src/main/java/appline/pages/MainPage.java
+++ src/main/java/appline/pages/MainPage.java
@@ -8,7 +8,7 @@ public class MainPage extends BasePage {
     @FindBy(xpath = "//span[contains(text(),'Ипотека')]")
     WebElement toMortgage;
 
-    @FindBy(xpath = "//li[@class='lg-menu__sub-item']//a[(text() = 'Ипотека на готовое жильё')]")
+    @FindBy(xpath = "//li[@class='lg-menu__sub-item']//a[text() = 'Ипотека на готовое жильё']")
     WebElement clickMortgage;
 
     public void goToMortgage() {
